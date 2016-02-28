911bbd962875ee48327f85f5ff12f65ed7374ce8
Bruce Fonseca <lennon.bruce@gmail.com>
2016-02-28 12:44:41 -0300

    Nav acordos com toggle()

--- application/views/includes/nav_acordos.php
+++ application/views/includes/nav_acordos.php
@@ -3,9 +3,11 @@
 	<div class="menu-side-bar">
 		<span class="glyphicon glyphicon-menu-hamburger btn-lg" aria-hidden="true"></span>
 	</div>
-	
 
-<?php foreach ($menu_planta->result() as $menu): ?>
+	
+<?php 
+
+foreach ($menu_planta->result() as $menu): ?>
 	<li class="list-plantas">
 	    <a href="#" class="link-plantas"><?php echo ucwords($menu->dsc_planta) ?><span class="caret"></span></a>
 	    <ul class="">
@@ -50,6 +52,49 @@
 
 		$('ul' , pai).toggle();
 	});
+
+	$( window ).resize(function() {
+  		$('.nav-acordos').css('height','100%');
+	});
+
+	$('.menu-side-bar span').on('click', function(){
+
+		if($(this).width()==20){//se estiver recolhido, então expande
+			$(this).css({
+				width : '20px;',
+				padding: '10px'
+			});
+			$('.menu-side-bar').css({
+				width : '100%'
+			});
+			$('.nav-acordos').css({
+				width : '15%'
+			});
+			
+		}else{//recolhe o menu
+			$(this).css({
+				width : '40px',
+				padding: '10px'
+			});
+			$('.menu-side-bar').css({
+				width : '40px'
+			});
+			$('.nav-acordos').css({
+				width : '40px'
+			});
+		}
+
+		//redimenciona a div conteudo
+		resize_div_conteudo();
+	});
+
+	function resize_div_conteudo(){
+		var w_nav =	$('.nav-acordos').width();
+		var w_con = $(window).width() - w_nav - 1;
+
+		$('.nav.nav-tabs').css('width' , w_con);
+		$('.conteudo-principal').css('width' , w_con);
+	}
 
 
 	// $('.list-plantas').on('click', function(){
